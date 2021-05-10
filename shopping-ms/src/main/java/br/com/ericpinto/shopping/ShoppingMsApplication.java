d0a48c84d8b31cdc2bf678bbcd8b6ecfbea612ed
Éric Pinto <ericgrandopinto@gmail.com>
2021-05-10 14:19:51 -0300

    first commit

---
+++ shopping-ms/src/main/java/br/com/ericpinto/shopping/ShoppingMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ericpinto.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMsApplication.class, args);
+    }
+
+}
---
+++ shopping-ms/src/test/java/br/com/ericpinto/shopping/ShoppingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ericpinto.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
